5504291e539511e50c682817e3cf71b7ea37b060
fakhryfernanda <mff023@gmail.com>
2023-02-14 22:18:42 +0700

    (+) single post route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('posts/{slug}', function ($slug) {
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
+})->where('post', '[A-z\-]+');
