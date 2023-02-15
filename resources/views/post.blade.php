f6e108308ae07429ec205c8c1006dbf85f2d2838
fakhryfernanda <mff023@gmail.com>
2023-02-15 10:43:57 +0700

    (~) using blade layouting

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('layout')
+
+@section('content')
     <article>
         <h1>{{ $post->title }}</h1>
 
@@ -16,10 +10,4 @@
     </article>
 
     <a href="/">Go back</a>
-</body>
-</html>
-
-{{-- @php
-    echo $post;    
-@endphp --}}
-{{-- <?= $post; ?> --}}
+@endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('layout')
+
+@section('content')
     @foreach ($posts as $post)
     <article>
         <h1>
             <a href="/posts/{{ $post->slug }}">
-                    {{ $post->title }}
-                </a>
-            </h1>
+                {{ $post->title }}
+            </a>
+        </h1>
 
-            {!! $post->excerpt !!}
-        </article>
+        {!! $post->excerpt !!}
+    </article>
     @endforeach
-</body>
-</html>
+@endsection
