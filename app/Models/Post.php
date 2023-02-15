d4ac4b996840d185b71f841d1bf1abece05a8f9f
fakhryfernanda <mff023@gmail.com>
2023-02-15 09:36:11 +0700

    (~) post as object

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Support\Facades\File;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 // use Illuminate\Database\Eloquent\Factories\HasFactory;
 // use Illuminate\Database\Eloquent\Model;
@@ -12,11 +13,41 @@ class Post // extends Model
 {
     // use HasFactory;
 
+    public $title;
+    public $slug;
+    public $body;
+    public $excerpt;
+    public $date;
+
+    public function __construct($title, $slug, $body, $excerpt, $date)
+    {
+        $this->title = $title;
+        $this->slug = $slug;
+        $this->body = $body;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+    }
+    
     public static function all()
     {
         $files = File::files(resource_path("posts/"));
+        // $posts = [];
+
+        $posts = array_map(function($file) {
+            $document = YamlFrontMatter::parseFile($file);
+
+            return new Post(
+                $document->title,
+                $document->slug,
+                $document->body(),
+                $document->excerpt,
+                $document->date
+            );
+        }, $files);
         
-        return array_map(fn($file) => $file->getContents(), $files);
+        // return array_map(fn($file) => $file->getContents(), $files);
+        
+        return $posts;
     }
 
     public static function find($slug)
