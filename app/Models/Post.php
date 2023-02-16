3125169bf6e5544b62045a79575c9a09b79a314c
fakhryfernanda <mff023@gmail.com>
2023-02-16 20:59:12 +0700

    (+) eloquent relation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
