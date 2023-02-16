40adbec7efb62c0ec18e023a7a8d768966ff6912
fakhryfernanda <mff023@gmail.com>
2023-02-16 20:58:38 +0700

    (+) category name

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,9 @@
 @section('content')
     <article>
         <h1>{{ $post->title }}</h1>
+        <p>
+            In category <a href="#">{{ $post->category->name }}</a>
+        </p>
 
         <div>
             {!! $post->body !!}
