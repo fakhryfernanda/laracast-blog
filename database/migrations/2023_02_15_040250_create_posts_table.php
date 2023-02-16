a8ef754fca45e86517cb12adff83f845445193d7
fakhryfernanda <mff023@gmail.com>
2023-02-16 20:58:50 +0700

    (+) slug and category_id

--- database/migrations/2023_02_15_040250_create_posts_table.php
+++ database/migrations/2023_02_15_040250_create_posts_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id');
+            $table->string('slug')->unique();
             $table->string('title');
             $table->text('excerpt');
             $table->text('body');
