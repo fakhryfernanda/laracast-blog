b944ba605c42c346fca24f3791348b1d9cf27336
fakhryfernanda <mff023@gmail.com>
2023-02-16 21:24:20 +0700

    (+) show author of the post

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
@@ -31,6 +32,13 @@ Route::get('posts/{post:slug}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category){
     return view('posts', [
         'posts' => $category->posts,
+        'categories' => Category::all()
+    ]);
+});
+
+Route::get('authors/{author:username}', function (User $author){
+    return view('posts', [
+        'posts' => $author->posts,
         'categories' => Category::all()
     ]);
 });
