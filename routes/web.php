ce1ad8e2a3b4ab4b92629921e54a292d812f1d8a
fakhryfernanda <mff023@gmail.com>
2023-02-17 08:52:52 +0700

    (-) author route

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,8 @@
 <?php
 
-use App\Models\Post;
-use App\Models\User;
-use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
-Route::get('authors/{author:username}', function (User $author){
-    return view('posts', [
-        'posts' => $author->posts,
-    ]);
-});
