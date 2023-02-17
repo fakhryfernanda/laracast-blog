76d384f0ddc0bca6ac3d528d69ddaaaebec5d2b8
fakhryfernanda <mff023@gmail.com>
2023-02-17 08:52:32 +0700

    (+) filter for authors

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -26,12 +26,19 @@ class Post extends Model
         $query->when($filters['search'] ?? false, fn($query, $search) =>
             $query
                 ->where('title', 'like', '%' . $search . '%')
-                ->orWhere('body', 'like', '%' . $search . '%'));
+                ->orWhere('body', 'like', '%' . $search . '%')
+        );
 
         $query->when($filters['category'] ?? false, fn($query, $category) =>
             $query->whereHas('category', fn ($query) =>
                 $query->where('slug', $category)
             )
         );
+
+        $query->when($filters['author'] ?? false, fn($query, $author) =>
+        $query->whereHas('author', fn ($query) =>
+            $query->where('username', $author)
+        )
+    );
     }
 }
