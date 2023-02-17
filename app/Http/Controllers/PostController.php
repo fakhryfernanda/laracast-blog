d13a6f580d6d25b3911216a472d7b07009c8366b
fakhryfernanda <mff023@gmail.com>
2023-02-17 07:30:41 +0700

    (+) PostController

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return view('posts', [
+            'posts' => Post::latest()->filter(request(['search']))->get(),
+            'categories' => Category::all()
+        ]);
+    }
+
+    public function show(Post $post)
+    {
+        return view('post', [
+            'post' => $post
+        ]);
+    }
+}
