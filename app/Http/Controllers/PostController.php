29ae68cfcd652d5503f4d271a0ad11e74fabe858
fakhryfernanda <mff023@gmail.com>
2023-02-17 08:46:30 +0700

    move all posts view to separate folder

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,14 +10,14 @@ class PostController extends Controller
 {
     public function index()
     {
-        return view('posts', [
+        return view('posts.index', [
             'posts' => Post::latest()->filter(request(['search', 'category']))->get()
         ]);
     }
 
     public function show(Post $post)
     {
-        return view('post', [
+        return view('posts.show', [
             'post' => $post
         ]);
     }
