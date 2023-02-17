d9153d280267dab2a2fd1b1fbc2628fcd600a7a3
fakhryfernanda <mff023@gmail.com>
2023-02-17 08:34:47 +0700

    (~) query uri

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -23,7 +23,7 @@
     
                     @foreach ($categories as $category)
                         <x-dropdown-item
-                            href="/categories/{{ $category->slug }}"
+                            href="/?category={{ $category->slug }}"
                             :active='request()->is("categories/{$category->slug}")'
                         >
                             {{ ucwords($category->name) }}
