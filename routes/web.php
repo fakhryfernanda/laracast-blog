918cdeddc863063bda2bf88a346696180757b8ab
fakhryfernanda <mff023@gmail.com>
2023-02-17 14:45:59 +0700

    (+) route for adding comment

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,12 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\SessionController;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\PostCommentController;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+Route::post('posts/{post:slug}/comments', [PostCommentController::class, 'store']);
+
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
