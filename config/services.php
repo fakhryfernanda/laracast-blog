bd0a2148c57d396956368c8802c58d49c48e2d20
fakhryfernanda <mff023@gmail.com>
2023-02-17 17:33:09 +0700

    (+) setup newsletter

--- config/services.php
+++ config/services.php
@@ -31,4 +31,11 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'mailchimp' => [
+        'key' => env('MAILCHIMP_KEY'),
+        'lists' => [
+            'subscribes' => env('MAILCHIMP_LIST_SUBSCRIBERS')
+        ]
+    ],
+
 ];
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,30 @@
 <?php
 
+use MailchimpMarketing\ApiClient;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\SessionController;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentController;
+
+Route::get('/ping', function () {
+    $mailchimp = new ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us9'
+    ]);
+
+    // $response = $mailchimp->ping->get();
+    $response = $mailchimp->lists->getAllLists();
+
+    dd($response);
+    print_r($response);
+});
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
